6173abfceee2c5857ba24572c238f202f6923bb1
Ibragim Yusupov <70488743+IbragimYusupov@users.noreply.github.com>
2022-11-02 14:51:10 +0100

    /[employeeId]/index.ts added temporary data
    Added temporary data for testing. Ideally data should not be duplicated and all endpoints should be able to access to same data.

---
+++ xam_webapp_2022_student/oppgave_3/pages/api/employees/[employeeId]/index.ts
@@ -0,0 +1,102 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const employees = [
+    {
+      id: 1,
+      name: 'Trude',
+      rules: 'days:123',
+    },
+    {
+      id: 2,
+      name: 'Lars',
+      rules: '*',
+    },
+    {
+      id: 3,
+      name: 'Finn',
+      rules: '*',
+    },
+    {
+      id: 4,
+      name: 'Kåre',
+      rules: 'days:*|week:odd',
+    },
+    {
+      id: 5,
+      name: 'Olav',
+      rules: '*',
+    },
+    {
+      id: 6,
+      name: 'Sebastian',
+      rules: '*',
+    },
+    {
+      id: 7,
+      name: 'Trine',
+      rules: 'days:*|week:even',
+    },
+    {
+      id: 8,
+      name: 'Simen',
+      rules: 'days:24',
+    },
+    {
+      id: 9,
+      name: 'Ali',
+      rules: 'days:1',
+    },
+    {
+      id: 10,
+      name: 'Josefine',
+      rules: 'days:*|week:3',
+    },
+    {
+      id: 11,
+      name: 'Nora',
+      rules: 'days:*|week:even',
+    },
+    {
+      id: 12,
+      name: 'Oline',
+      rules: 'days:*|week:even',
+    },
+    {
+      id: 13,
+      name: 'Susanne',
+      rules: 'days:*|week:odd',
+    },
+    {
+      id: 14,
+      name: 'Sharek',
+      rules: 'days:*',
+    },
+    {
+      id: 15,
+      name: 'Carlos',
+      rules: 'days:*|week:odd',
+    },
+  ]
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+
+  switch (req.method?.toLowerCase()) {
+    case 'get':
+      const id = req.query.employeeId
+      return res.status(200).json({ status: true, data: { resources: `employees/${id}/index`}})  
+    default:
+      return res
+      .status(400)
+      .json({
+        success: false, 
+        error: { 
+          type: 'object', 
+          status:'400', 
+          message:'Method not allowed'
+        },
+      });
+  }
+}
