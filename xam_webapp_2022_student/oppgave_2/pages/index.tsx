03299f281367d903e877d0306bf527b79ab625a2
Omar <omarinio95@yahoo.com>
2022-11-05 00:21:36 +0100

    jobbet hele dagen med filtrering og neste fikk det

--- xam_webapp_2022_student/oppgave_2/pages/index.tsx
+++ xam_webapp_2022_student/oppgave_2/pages/index.tsx
@@ -30,32 +30,95 @@ const Home: NextPage = () => {
   }, [])
 
   if (isLoading) {
-    return <p>Henter data ...</p>
+    return <p>Henter data..</p>
   }
 
   if (isError) {
-    return <p>Noe gikk galt ...</p>
+    return <p>Noe gikk galt..</p>
+  }
+
+  function handleChange(event) {
+    console.log(event.target.value)
+
+    if (event.target.value == 'age') {
+      console.log(
+        data.students.reduce((acc, cur) => {
+          if (acc[cur.age]) {
+            acc[cur.age]++
+          } else {
+            acc[cur.age] = 1
+          }
+          return acc
+        }, {})
+      )
+    }
+
+    if (event.target.value == 'gender') {
+      console.log(
+        data.students.reduce((acc, cur) => {
+          if (acc[cur.gender]) {
+            acc[cur.gender]++
+          } else {
+            acc[cur.gender] = 1
+          }
+          return acc
+        }, {})
+      )
+    }
+
+    if (event.target.value == 'group') {
+      console.log(
+        data.students.reduce((acc, cur) => {
+          if (acc[cur.group]) {
+            acc[cur.group]++
+          } else {
+            acc[cur.group] = 1
+          }
+          return acc
+        }, {})
+      )
+    }
   }
 
   return (
     <main>
       <h1>Student gruppering</h1>
       <section>
-        <label htmlFor="alfabetisk">Alfabetisk</label>
+        <label htmlFor="ingen">Ingen</label>
         <input
           name="filter"
-          id="alfabetisk"
+          id="ingen"
+          value="ingen"
           type="radio"
           defaultChecked
+          onChange={handleChange}
         ></input>
         <label htmlFor="alder">Alder</label>
-        <input name="filter" id="alder" type="radio"></input>
+        <input
+          name="filter"
+          id="alder"
+          value="age"
+          type="radio"
+          onChange={handleChange}
+        ></input>
         <label htmlFor="kjønn">Kjønn</label>
-        <input name="filter" id="kjønn" type="radio"></input>
+        <input
+          name="filter"
+          id="kjønn"
+          value="gender"
+          type="radio"
+          onChange={handleChange}
+        ></input>
         <label htmlFor="klasse">Klasse</label>
-        <input name="filter" id="klasse" type="radio"></input>
+        <input
+          name="filter"
+          id="klasse"
+          value="group"
+          type="radio"
+          onChange={handleChange}
+        ></input>
       </section>
-
+      {/* <p className="antall">Antall studenter: {data.students.length}</p> */}
       {data?.students
         ?.sort((a, b) => (a.name > b.name ? 1 : -1))
         .map((student) => (
