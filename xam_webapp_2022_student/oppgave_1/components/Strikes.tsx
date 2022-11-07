981564a4a102fa90961b79c9ab124af717de2f9c
Ibragim Yusupov <70488743+IbragimYusupov@users.noreply.github.com>
2022-11-07 21:27:38 +0100

    Changed guess datatype from number to string

--- xam_webapp_2022_student/oppgave_1/components/Strikes.tsx
+++ xam_webapp_2022_student/oppgave_1/components/Strikes.tsx
@@ -1,6 +1,6 @@
 // TODO: Her er det bugs
 
-export type Strike = { icon: string; guess: number}
+export type Strike = { icon: string; guess: string}
 
 export default function Strikes({ strikes }: { strikes: Strike[] }) {
   return (
