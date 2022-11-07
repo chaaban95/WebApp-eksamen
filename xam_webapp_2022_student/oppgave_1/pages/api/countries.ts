42328a3af69a8e8a7165243a6010935f90396e4c
Ibragim Yusupov <70488743+IbragimYusupov@users.noreply.github.com>
2022-11-07 14:33:11 +0100

    api/countries now works properly

--- xam_webapp_2022_student/oppgave_1/pages/api/countries.ts
+++ xam_webapp_2022_student/oppgave_1/pages/api/countries.ts
@@ -8,5 +8,5 @@ export default async function handler(
   res: NextApiResponse<any>
 ) {
   const country = countries[Math.floor(Math.random() * countries.length)]
-  return res.status(404).json({ success: true, data: country })
+  return res.status(200).json({ success: true, data: country })
 }
