3ed9a700a208c8b3af7419fc323122a14af5de91
Ibragim Yusupov <70488743+IbragimYusupov@users.noreply.github.com>
2022-11-11 18:35:40 +0100

    api/demo adds employee data to db

--- xam_webapp_2022_student/oppgave_3/pages/api/demo/index.ts
+++ xam_webapp_2022_student/oppgave_3/pages/api/demo/index.ts
@@ -1,29 +1,55 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { employees } from '../../../data/employees';
+import { employees as employeesData } from '../../../data/employees'
 import lunchData from '../../../data/lunch.json'
 
+import { Prisma, PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient({ log: ['query'] })
 
 export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<any>
-  ) {
-  
-    switch (req.method?.toLowerCase()) {
-  
-      case 'post':
-        return res.status(200).json({ status: true, data: {method: req.method, resources: '/employees/index', employees}})
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  switch (req.method?.toLowerCase()) {
+    case 'post':
+      async function main() {
+        let employees: Prisma.EmployeeCreateInput[] = employeesData
 
-      default:
-        return res
-        .status(400)
-        .json({
-          success: false, 
-          error: { 
-            type: 'object', 
-            status:'400', 
-            message:'Method not allowed'
-          },
-        });
-    }
+        await Promise.all(
+          employees.map(async (employee) => {
+            await prisma.employee.create({
+              data: employee,
+            })
+          })
+        )
+      }
+
+      main()
+        .then(async () => {
+          await prisma.$disconnect()
+        })
+        .catch(async (e) => {
+          console.error(e)
+          await prisma.$disconnect()
+          process.exit(1)
+        })
+
+      return res.status(200).json({
+        status: true,
+        data: {
+          method: req.method,
+          message: 'Employees have been added!',
+        },
+      })
+
+    default:
+      return res.status(400).json({
+        success: false,
+        error: {
+          type: 'object',
+          status: '400',
+          message: 'Method not allowed',
+        },
+      })
   }
+}
