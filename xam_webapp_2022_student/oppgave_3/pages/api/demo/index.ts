6c8a4bb90e989fb1aa81ac7912e7b422bc227a81
Omar <omarinio95@yahoo.com>
2022-11-28 02:12:37 +0100

    prøvd litt

--- xam_webapp_2022_student/oppgave_3/pages/api/demo/index.ts
+++ xam_webapp_2022_student/oppgave_3/pages/api/demo/index.ts
@@ -1,8 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Prisma, PrismaClient } from '@prisma/client'
 
-import { employees as employeesData } from '../../../data/employees'
-import lunchData from '../../../data/lunch.json'
+import { employees } from '../../../data/employees'
+import lunch from '../../../data/lunch.json'
 
 const prisma = new PrismaClient({ log: ['query'] })
 
@@ -10,70 +10,37 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  switch (req.method?.toLowerCase()) {
-    case 'get':
-      async function main() {
-        // Create employees
-        let employees: Prisma.EmployeeCreateInput[] = employeesData
-
-        await Promise.all(
-          employees.map(async (employee) => {
-            await prisma.employee.create({
-              data: employee,
-            })
-          })
-        )
-
-        // Create lunch
-        const Lunch = await prisma.lunch.create({
-          data: {},
-        })
-
-        // For loop for getting week and creating week in db.
-        // for (const weeks of Object.values(lunchData.year)) {
-        //   const week = await prisma.week.create({
-        //     data: {
-        //       week: week,
-        //     },
-        //   })
-        // }
-
-        // const weeks = Object.entries(lunchData.year)
-
-        // const createWeek = await prisma.week.create({
-        //   data: {
-        //     id: ,
-        //     week: 1
-        //   },
-        // })
-      }
-
-      main()
-        .then(async () => {
-          await prisma.$disconnect()
-        })
-        .catch(async (e) => {
-          console.error(e)
-          await prisma.$disconnect()
-          process.exit(1)
-        })
-
-      return res.status(200).json({
-        status: true,
-        data: {
-          method: req.method,
-          message: 'Data has been added!',
-        },
+  async function main() {
+    // Create employees
+    employees.map(async (employee) => {
+      await prisma.employee.create({
+        data: employee,
       })
+    })
 
-    default:
-      return res.status(400).json({
-        success: false,
-        error: {
-          type: 'object',
-          status: '400',
-          message: 'Method not allowed',
-        },
-      })
+    // Create lunch
+    // Object?.entries(lunch.year)?.map(async ([key, value]) => {
+    //   await prisma.lunch.create({
+    //     data:
+    //   })
+    // })
+    // Create weeks
+    // Object?.entries(lunch.year)?.map(async ([key, value]) => {
+    //   Object?.entries(value.week)?.map(async ([key2, value2]) => {
+    //     await prisma.week.create({
+    //       data: {},
+    //     })
+    //   })
+    // })
   }
+
+  main()
+    .catch((e) => {
+      console.error(e)
+      process.exit(1)
+    })
+    .finally(async () => {
+      console.log('Added')
+      await prisma.$disconnect()
+    })
 }
