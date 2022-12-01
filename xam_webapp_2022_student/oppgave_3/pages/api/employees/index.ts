1c27700a2ef0df5bae9bb91f33b2bf603429838a
Ibragim Yusupov <70488743+IbragimYusupov@users.noreply.github.com>
2022-12-01 11:53:37 +0100

    api/employees gets all employees from db

--- xam_webapp_2022_student/oppgave_3/pages/api/employees/index.ts
+++ xam_webapp_2022_student/oppgave_3/pages/api/employees/index.ts
@@ -1,1 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../lib/db'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  if (req.method === 'GET') {
+    const employees = await prisma.employee.findMany()
+
+    return res.status(200).json({ success: true, employees })
+  } else {
+    return res.status(405).json({ success: false, error: 'Method not allowed' })
+  }
+}
--- xam_webapp_2022_student/oppgave_3/pages/api/weeks/index.ts
+++ xam_webapp_2022_student/oppgave_3/pages/api/weeks/index.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/db'
 
 export default async function handler(
   req: NextApiRequest,
@@ -6,6 +7,7 @@ export default async function handler(
 ) {
   if (req.method === 'GET') {
     const weeks = await prisma.week.findMany()
+
     return res.status(200).json({ success: true, weeks })
   } else {
     return res.status(405).json({ success: false, error: 'Method not allowed' })
