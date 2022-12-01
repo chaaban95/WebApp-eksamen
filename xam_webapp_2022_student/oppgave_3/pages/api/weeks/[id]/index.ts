b63ae7005f5a0c5458ed982314f2bee927980748
Omar <omarinio95@yahoo.com>
2022-12-01 19:47:26 +0100

    pull

--- xam_webapp_2022_student/oppgave_3/pages/api/weeks/[id]/index.ts
+++ xam_webapp_2022_student/oppgave_3/pages/api/weeks/[id]/index.ts
@@ -16,15 +16,26 @@ export default async function handler(
       //   },
       // })
 
+<<<<<<< HEAD
       // Using raw raw queries instead
       const week = await prisma.$queryRaw`SELECT * FROM week WHERE week = ${id}`
+=======
+      // Todo: get the right data
+      // Using raw raw queries instead
+      const days = await prisma.$queryRaw`SELECT * FROM day WHERE weekId = ${id}`
+>>>>>>> 74614da67d42c63cf0501adef09db94b1bf27ab9
 
       return res.status(200).json({
         status: true,
         data: {
           method: req.method,
+<<<<<<< HEAD
           resources: 'api/weeks/${id}',
           week,
+=======
+          resources: 'api/weeks/',
+          data: days,
+>>>>>>> 74614da67d42c63cf0501adef09db94b1bf27ab9
         },
       })
 
