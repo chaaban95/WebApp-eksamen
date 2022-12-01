d10b94ea7d7f25b91993045b72c11fc83a3be332
Ibragim Yusupov <70488743+IbragimYusupov@users.noreply.github.com>
2022-12-01 11:10:59 +0100

    api/demo/weeks/[id], dynamic api route, can get one week

---
+++ xam_webapp_2022_student/oppgave_3/pages/api/weeks/[id]/index.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method?.toLowerCase()) {
+    case 'get':
+      const { id } = req.query
+
+      // Does not work not sure why??
+      // const week = await prisma.week.findUnique({
+      //   where: {
+      //     week: id,
+      //   },
+      // })
+
+      // Using raw raw queries instead
+      const week = await prisma.$queryRaw`SELECT * FROM week WHERE week = ${id}`
+
+      return res.status(200).json({
+        status: true,
+        data: {
+          method: req.method,
+          resources: 'api/weeks/${id}',
+          data: week,
+        },
+      })
+
+    default:
+      return res.status(400).json({
+        success: false,
+        error: {
+          type: 'Object',
+          status: '400',
+          message: 'Method not allowed',
+        },
+      })
+  }
+}
