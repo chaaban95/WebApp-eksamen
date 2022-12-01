2350f74e318bc2e66c41fa94348cb16d95b39257
Omar <omarinio95@yahoo.com>
2022-12-01 03:50:20 +0100

    fix api

---
+++ xam_webapp_2022_student/oppgave_3/api/weeks.ts
@@ -0,0 +1,11 @@
+import fetcher from '../lib/fetch'
+
+const BASE_URL = '/api'
+const WEEKS_URL = `${BASE_URL}/weeks`
+
+export const getWeeks = (options: any) => {
+  return fetcher(WEEKS_URL, {
+    method: 'GET',
+    ...options,
+  })
+}
--- xam_webapp_2022_student/oppgave_3/pages/api/weeks/index.ts
+++ xam_webapp_2022_student/oppgave_3/pages/api/weeks/index.ts
@@ -1,29 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+import prisma from '../../../lib/db'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  switch (req.method?.toLowerCase()) {
-    case 'get':
-      const weeks = await prisma.week.findMany()
-
-      return res.status(200).json({
-        status: true,
-        data: { method: req.method, resources: 'api/weeks/index', data: weeks },
-      })
-
-    default:
-      return res.status(400).json({
-        success: false,
-        error: {
-          type: 'Object',
-          status: '400',
-          message: 'Method not allowed',
-        },
-      })
+  if (req.method === 'GET') {
+    const weeks = await prisma.week.findMany()
+    return res.status(200).json({ success: true, weeks })
+  } else {
+    return res.status(405).json({ success: false, error: 'Method not allowed' })
   }
 }
