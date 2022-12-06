b04bec7e598e2f5a4f2768642c8cacf5fc50405d
Ibragim Yusupov <70488743+IbragimYusupov@users.noreply.github.com>
2022-12-06 19:33:09 +0100

    Updated statuscode

--- xam_webapp_2022_student/oppgave_3/pages/api/employees/create/index.ts
+++ xam_webapp_2022_student/oppgave_3/pages/api/employees/create/index.ts
@@ -8,7 +8,7 @@ export default async function handler(
   if (req.method === 'POST') {
     const employee = await employeesController.createEmployee(req, res)
 
-    return res.status(200).json({ success: true, employee })
+    return res.status(201).json({ success: true, employee })
   } else {
     return res.status(405).json({ success: false, error: 'Method not allowed' })
   }
